a83001c98d80a9b08de54d2b1ba57faf208a634c
David Chase <david.chase4@gmail.com>
2017-04-15 14:06:07 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,63 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner = []
+  data.each do |season_number, season_data|
+    if season == season_number
+      season_data.each do |contestant_data|
+        if contestant_data["status"] == "Winner"
+          winner << contestant_data["name"].split(' ')
+        end
+      end
+    end
+  end
+  winner[0][0]
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  name = []
+  data.each do |season_number, season_data|
+    season_data.each do |contestant_data|
+      if occupation == contestant_data["occupation"]
+        name << contestant_data["name"]
+      end
+    end
+  end
+  name[0]
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = []
+  data.each do |season_number, season_data|
+    season_data.each do |contestant_data|
+      if hometown == contestant_data["hometown"]
+        count << contestant_data["name"]
+      end
+    end
+  end
+  count.length
 end
 
 def get_occupation(data, hometown)
-  # code here
+  first_occupation = []
+  data.each do |season_number, season_data|
+    season_data.find do |contestant_data|
+      if hometown == contestant_data["hometown"]
+        first_occupation << contestant_data["occupation"]
+      end
+    end
+  end
+  first_occupation[0]
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  age_array = []
+  data.each do |season_number, season_data|
+    if season == season_number
+      season_data.each do |contestant_data|
+        age_array << contestant_data["age"].to_i
+      end
+    end
+  end
+  (age_array.inject do |sum, age|
+    sum + age
+  end.to_f / age_array.size).round
 end
